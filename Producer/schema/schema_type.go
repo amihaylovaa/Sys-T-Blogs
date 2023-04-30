6c053d649f99d160b82f874e0e3ce9da106a2258
amihaylovaa <aggmihaylova@gmail.com>
2023-04-30 23:36:24 +0300

    add schema contants

---
+++ Producer/schema/schema_type.go
@@ -0,0 +1,8 @@
+package schema
+
+type SchemaType string
+
+const (
+	BLOG_DTO_SCHEMA    SchemaType = "schema/blog_dto_schema.json"
+	COMMENT_DTO_SCHEMA SchemaType = "schema/comment_dto_schema.json"
+)
