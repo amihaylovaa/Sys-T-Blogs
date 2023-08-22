be3a8b16d0fb303e1b495739fb81ca3b7652aeae
amihaylovaa <aggmihaylova@gmail.com>
2023-08-22 21:53:56 +0300

    change spring version and add groupId

--- Consumer/src/main/java/aggm/personal/consumer/listener/Listener.java
+++ Consumer/src/main/java/aggm/personal/consumer/listener/Listener.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Listener {
 
-    @KafkaListener(topics = "comments")
+    @KafkaListener(topics = "comments", groupId = "groupId")
     void commentsListener(String data) {
 
     }
 
-    @KafkaListener(topics = "blogs")
+    @KafkaListener(topics = "blogs", groupId = "groupId")
     void blogsListener(String data) {
 
     }
