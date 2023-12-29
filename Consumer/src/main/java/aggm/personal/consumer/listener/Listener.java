afea63c9fe6179de5846bcbec5d8dc7613a87a26
amihaylovaa <aggmihaylova@gmail.com>
2023-12-29 22:30:35 +0200

    save events

--- Consumer/src/main/java/aggm/personal/consumer/listener/Listener.java
+++ Consumer/src/main/java/aggm/personal/consumer/listener/Listener.java
@@ -1,5 +1,9 @@
 package aggm.personal.consumer.listener;
 
+import aggm.personal.consumer.domain.Blog;
+import aggm.personal.consumer.dto.CommentDto;
+import aggm.personal.consumer.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -8,17 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class Listener {
 
+    @Autowired
+    private BlogService blogService;
+
 @KafkaListener(
         topicPartitions = @TopicPartition(topic = "comments",
         partitionOffsets = { @PartitionOffset(partition = "0", initialOffset = "0")}))
-    void commentsListener(String data) {
-        // SAVE
+    void commentsListener(CommentDto commentDto) {
+        blogService.saveComment(commentDto);
     }
 
     @KafkaListener(
             topicPartitions = @TopicPartition(topic = "blogs",
             partitionOffsets = { @PartitionOffset(partition = "0", initialOffset = "0")}))
-    void blogsListener(String data) {
-        // SAVE
+    void blogsListener(Blog blog) {
+        blogService.saveBlog(blog);
     }
 }
