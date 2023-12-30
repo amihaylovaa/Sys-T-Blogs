b0e788a5b18c33082fa3fc0c31f53261ea41c8f2
amihaylovaa <aggmihaylova@gmail.com>
2023-12-30 13:20:08 +0200

    update producer service version, add user

--- Producer/blog_dto.go
+++ Producer/blog_dto.go
@@ -4,5 +4,6 @@ type BlogDto struct {
 	Title          string   `json: "title"`
 	Subtitle       string   `json: "subtitle"`
 	Content        string   `json: "content"`
+	User           string   `json: "user"`
 	AttachmentsUrl []string `json: "attachmentsUrl"`
 }
