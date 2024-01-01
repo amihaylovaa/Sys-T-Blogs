7b419290d2e710cb8422140abce03daf31784b2a
amihaylovaa <aggmihaylova@gmail.com>
2024-01-01 21:15:27 +0200

    add simple converter

--- Consumer/src/main/java/aggm/personal/consumer/service/BlogService.java
+++ Consumer/src/main/java/aggm/personal/consumer/service/BlogService.java
@@ -1,16 +1,15 @@
 package aggm.personal.consumer.service;
 
+import aggm.personal.consumer.converter.Converter;
 import aggm.personal.consumer.dto.BlogDto;
 import aggm.personal.consumer.dto.CommentDto;
 import aggm.personal.consumer.domain.Blog;
 import aggm.personal.consumer.domain.Comment;
 import aggm.personal.consumer.exception.DocumentNotFoundException;
 import aggm.personal.consumer.repository.BlogRepository;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -20,7 +19,7 @@ public class BlogService {
     private BlogRepository blogRepository;
 
     public void saveBlog(BlogDto blogDto) {
-        Blog blog = convertBlogDtoToBlog(blogDto);
+        Blog blog = Converter.convertBlogDtoToBlog(blogDto);
 
         blogRepository.save(blog);
     }
@@ -31,39 +30,11 @@ public class BlogService {
         if (!blogOptional.isPresent()) {
             throw new DocumentNotFoundException();
         }
-        Comment comment = convertCommentDtoToComment(commentDto);
+        Comment comment = Converter.convertCommentDtoToComment(commentDto);
 
         Blog blog = blogOptional.get();
         blog.addComment(comment);
         // TODO - is it efficient?
         blogRepository.save(blog);
-    }
-
-    private Comment convertCommentDtoToComment(CommentDto dto) {
-        Comment comment = new Comment();
-
-        // TODO - add converter
-        comment.setContent(dto.getContent());
-        comment.setTitle(comment.getTitle());
-        comment.setUser(dto.getUser());
-        comment.setPublishingTimestamp(LocalDateTime.now());
-        comment.setAttachmentsUrl(dto.getAttachmentsUrl());
-
-        return comment;
-    }
-
-    private Blog convertBlogDtoToBlog(BlogDto dto) {
-        Blog blog = new Blog();
-
-        // TODO - add converter
-        blog.setBlogId(new ObjectId());
-        blog.setContent(dto.getContent());
-        blog.setTitle(dto.getTitle());
-        blog.setSubtitle(dto.getSubtitle());
-        blog.setUser(dto.getUser());
-        blog.setAttachmentsUrl(dto.getAttachmentsUrl());
-        blog.setPublishingTimestamp(LocalDateTime.now());
-
-        return blog;
     }
 }
